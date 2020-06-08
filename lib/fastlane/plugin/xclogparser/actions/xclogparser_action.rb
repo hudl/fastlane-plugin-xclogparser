30a642a6f35f597c5f0493d9ba51653f73d182ec
Josh Lesch <josh.r.lesch@gmail.com>
2020-06-08 11:08:02 -0500

    Blank plugin

--- lib/fastlane/plugin/xclogparser/actions/xclogparser_action.rb
+++ lib/fastlane/plugin/xclogparser/actions/xclogparser_action.rb
@@ -6,52 +6,7 @@ module Fastlane
   module Actions
     class XclogparserAction < Action
       def self.run(params)
-        Helper::XclogparserHelper.check_if_installed
-
-        command = params[:command].downcase
-
-        if command.eql?('parse')
-          options = params[:options]
-          if options == "" || options == [] || options == nil
-            UI.user_error!("--reporter and oreporterne of --file, --project, --workspace, --xcodeproj parameters is required.\nSee https://github.com/spotify/XCLogParser#parse-command for more information about the Parse command.")
-          end
-          options = format_options(options)
-        end
-
-        xclogparse_command = ['xclogparser', command, options].compact.join(" ")
-
-        begin
-          xclogparse_output = Action.sh(xclogparse_command)
-        rescue FastlaneCore::Interface::FastlaneShellError => e
-          if e.message.include?("//Logs/Build")
-            UI.error("Since Xcode 11, xcodebuild only generates the .xcactivitylog build logs when the option --resultBundlePath. If you're compiling with that command and not with Xcode, be sure to set that option to a valid path")
-          end
-          UI.user_error!(e.message)
-        end
-
-        if params[:zip_html_report]
-          report_path = parse_output_for_html_report(xclogparse_output)
-          report_directory = File.dirname(report_path)
-          report_time = Time.parse(File.basename(report_directory))
-          zip_name = "xclogparser_report_#{report_time.strftime("%Y-%m-%d-%H%M%S")}.zip"
-          root_dir = Dir.pwd
-          Dir.chdir(report_directory) do
-            Action.sh("zip -r -X #{File.join(root_dir, zip_name)} .")
-          end
-        end
-        xclogparse_output
-      end
-
-      def self.format_options(options)
-        if options.kind_of?(Array)
-          options.map! { |i| i.include?("--") ? i : "--#{i}" }
-        else
-          options.include?("--") ? options : "--#{options}"
-        end
-      end
-
-      def self.parse_output_for_html_report(unparsed_output)
-        unparsed_output.split("Report written to ")[1]
+        UI.message("The xclogparser plugin is working!")
       end
 
       def self.description
@@ -67,26 +22,6 @@ module Fastlane
       end
 
       def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :command,
-                                       description: "XCLogParse command. Run `xclogparse help` to list availabel commands",
-                                       default_value: "parse",
-                                       type: String),
-          FastlaneCore::ConfigItem.new(key: :options,
-                                       description: "Options for the XCLogParse command",
-                                       is_string: false,
-                                       optional: true,
-                                       verify_block: -> (value) {
-                                        case value when String; when Array;
-                                          else  UI.user_error! "Invalid option: #{value.inspect}, must be an Array or String"
-                                        end
-                                       }),
-          FastlaneCore::ConfigItem.new(key: :zip_html_report,
-                                       description: "Zip HTML report for transport",
-                                       is_string: false,
-                                       default_value: false,
-                                       optional: true)
-        ]
       end
 
       def self.is_supported?(platform)
